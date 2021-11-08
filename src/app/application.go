0376870c3d8e7c80f6c6ea8e8e85223922d984ec
Lozovoi Rodion <rodionlozovoi@gmail.com>
2021-11-08 22:27:14 +0200

    [WIP] : working on CREATE/UPDATE http handlers

--- src/app/application.go
+++ src/app/application.go
@@ -23,5 +23,7 @@ func StartApplication() {
 	atHandler := http.NewAccessTokenHandler(atService)
 
 	router.GET("/oauth/access_token/:access_token_id", atHandler.GetById)
+	router.POST("/oauth/access_token", atHandler.Create)
+	router.PUT("/oauth/access_token", atHandler.Update)
 	router.Run(":8080")
 }
--- src/http/http_access_token.go
+++ src/http/http_access_token.go
@@ -2,6 +2,7 @@ package http
 
 import (
 	"github.com/Lozovoi-Rodion/bookstore_oauth-api/src/domain/access_token"
+	"github.com/Lozovoi-Rodion/bookstore_oauth-api/src/domain/utils/errors"
 	"github.com/gin-gonic/gin"
 	"net/http"
 	"strings"
@@ -9,6 +10,8 @@ import (
 
 type AccessTokenHandler interface {
 	GetById(c *gin.Context)
+	Create(c *gin.Context)
+	Update(c *gin.Context)
 }
 
 type accessTokenHandler struct {
@@ -29,5 +32,37 @@ func (h *accessTokenHandler) GetById(c *gin.Context) {
 		c.JSON(err.Status, err)
 		return
 	}
-	c.JSON(http.StatusOK, accessToken )
+	c.JSON(http.StatusOK, accessToken)
+}
+
+func (h *accessTokenHandler) Create(c *gin.Context) {
+	var at access_token.AccessToken
+	if err := c.ShouldBindJSON(&at); err != nil {
+		restErr := errors.NewBadRequestError("invalid json body")
+		c.JSON(restErr.Status, restErr)
+		return
+	}
+
+	if err := h.service.Create(at); err != nil {
+		c.JSON(err.Status, err)
+		return
+	}
+
+	c.JSON(http.StatusCreated, at)
+}
+
+func (h *accessTokenHandler) Update(c *gin.Context) {
+	var at access_token.AccessToken
+	if err := c.ShouldBindJSON(&at); err != nil {
+		restErr := errors.NewBadRequestError("invalid json body")
+		c.JSON(restErr.Status, restErr)
+		return
+	}
+
+	if err := h.service.UpdateExpirationTime(at); err != nil {
+		c.JSON(err.Status, err)
+		return
+	}
+
+	c.JSON(http.StatusCreated, at)
 }
